ed2033aa01d2c81f55a4f787de75592d232e42fb
davepf <kamaamsafi@gmail.com>
2025-06-17 12:53:03 +0300

    Implement transaction saving in M-Pesa callback

---
+++ callback_url.php
@@ -0,0 +1,12 @@
+<?php
+$data = file_get_contents('php://input');
+$logFile = "mpesa_response.json";
+
+file_put_contents($logFile, $data, FILE_APPEND);
+
+// Optional: Insert into DB
+$response = json_decode($data, true);
+if (isset($response['Body']['stkCallback']['ResultCode']) && $response['Body']['stkCallback']['ResultCode'] == 0) {
+    // Payment successful - extract transaction details and store
+}
+?>
