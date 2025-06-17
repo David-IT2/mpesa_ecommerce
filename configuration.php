8614f477148be78e29d053b1239cdeee322fc5d9
davepf <kamaamsafi@gmail.com>
2025-06-17 13:59:36 +0300

    Add initial configuration for M-Pesa integration

---
+++ configuration.php
@@ -0,0 +1,7 @@
+<?php
+define('CONSUMER_KEY', 'YourConsumerKey');
+define('CONSUMER_SECRET', 'YourConsumerSecret');
+define('BUSINESS_SHORTCODE', '174379');
+define('PASSKEY', 'YourLipaNaMpesaPasskey');
+define('CALLBACK_URL', 'https://yourdomain.com/callback_url.php');
+?>
